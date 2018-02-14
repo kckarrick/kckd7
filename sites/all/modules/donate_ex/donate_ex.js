5813e92276e0f92ebca0b94ccb0397e16e1f43c3
Kevin <kevin@artistworks.com>
2018-02-13 22:55:53 -0800

    js fix

--- sites/all/modules/donate_ex/donate_ex.js
+++ sites/all/modules/donate_ex/donate_ex.js
@@ -7,12 +7,12 @@ Drupal.behaviors.donate_ex = {
     console.log('Inside donate_ex.js');
 
     jQuery('.form-item-submitted-amount-options').change(function () {
-      console.log('1' . this.value);
+      console.log('1: ' . this.value);
     });
 
-    jQuery('input[type=radio][name=submitted[amount_options]]').change(function () {
-      console.log('2' + this.value);
-    });
+    //jQuery('input[type=radio][name=submitted[amount_options]]').change(function () {
+    //  console.log('2' + this.value);
+    //});
 
   }
 }
