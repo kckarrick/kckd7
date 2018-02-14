a5785866e66c73e0d19a83546fe6c1a993658078
Kevin <kevin@artistworks.com>
2018-02-13 22:47:05 -0800

    debug+++

--- sites/all/modules/donate_ex/donate_ex.js
+++ sites/all/modules/donate_ex/donate_ex.js
@@ -6,11 +6,11 @@ Drupal.behaviors.donate_ex = {
 
     console.log('Inside donate_ex.js');
 
-    $('.form-item-submitted-amount-options').change(function () {
+    jQuery('.form-item-submitted-amount-options').change(function () {
       console.log('1' . this.value);
     }
 
-    $('input[type=radio][name=submitted[amount_options]]').change(function () {
+    jQuery('input[type=radio][name=submitted[amount_options]]').change(function () {
       console.log('2' + this.value);
     }
 
