c2a23cc6cd34b29cca8af1cf73bfe99f1af74eb1
Kevin <kevin@artistworks.com>
2018-02-13 23:36:42 -0800

    xfer value

--- sites/all/modules/donate_ex/donate_ex.js
+++ sites/all/modules/donate_ex/donate_ex.js
@@ -10,6 +10,23 @@
 
       $('input[type=radio][name="submitted[amount_options]"]').change(function () {
         console.log('value: ' + this.value);
+        theval = 0;
+        if (this.value == a1) {
+          theval = 20;
+        }
+        else if (this.value == a2) {
+          theval = 50;
+        }
+        else if (this.value == a3) {
+          theval = 100;
+        }
+        if (theval == 0) {
+          $('#edit-submitted-amount').value = '';
+          $('#edit-submitted-amount').focus();
+        }
+        else {
+          $('#edit-submitted-amount').value = theval;
+        }
       });
   //});
 
