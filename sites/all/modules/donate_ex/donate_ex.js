fb2176fe65dce9ba3d9cb5b08701a91836e8fdf6
Kevin <kevin@artistworks.com>
2018-02-13 23:26:37 -0800

    try this++

--- sites/all/modules/donate_ex/donate_ex.js
+++ sites/all/modules/donate_ex/donate_ex.js
@@ -2,24 +2,18 @@
 
 (function ($) {
 
-  console.log('Inside donate_ex.js');
+  Drupal.behaviors.donate_ex1 = {
+    attach: function (context, settings) {
 
-  //Drupal.behaviors.donate_ex1 = {
-  //  attach: function (context, settings) {
+  //$(document).ready(function () {
 
-  $(document).ready(function () {
-      console.log('Inside donate_ex.js');
-
-      $('.form-item-submitted-amount-options').change(function () {
-        console.log('1: ' + $(this).val());
-      });
 
       $('input[type=radio][name="submitted[amount_options]"]').change(function () {
-        console.log('2' + this.value);
+        console.log('value: ' + this.value);
       });
-  });
+  //});
 
-  //  }
-  //}
+    }
+  }
 
 })(jQuery);
